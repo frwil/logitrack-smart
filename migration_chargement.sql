cb7a0c6e2388d6f20ac2b8fed0913d8f090c0442
frwil <tagneffw@gmail.com>
2026-05-12 04:41:33 +0100

    Fix migration SQL: INT UNSIGNED pour matcher vehicule.id_vehicule

--- migration_chargement.sql
+++ migration_chargement.sql
@@ -2,8 +2,8 @@
 ALTER TABLE type_chargement_voyage ADD COLUMN unite_mesure VARCHAR(50) NOT NULL DEFAULT '' AFTER lib_type_chargement;
 
 CREATE TABLE IF NOT EXISTS capacite_vehicule_unite (
-    id_capacite_vehicule_unite INT AUTO_INCREMENT PRIMARY KEY,
-    id_vehicule INT NOT NULL,
+    id_capacite_vehicule_unite INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    id_vehicule INT UNSIGNED NOT NULL,
     unite_mesure VARCHAR(50) NOT NULL,
     capacite_max DECIMAL(10,2) NOT NULL,
     UNIQUE KEY uq_vehicule_unite (id_vehicule, unite_mesure),
